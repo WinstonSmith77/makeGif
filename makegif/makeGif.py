6b8efced436a9411e4d79a8ec4b70a4b4f59b857
Matthias Henning <matze_henning@gmx.de>
2024-03-15 20:12:39 +0100

    weiter

--- makegif/makeGif.py
+++ makegif/makeGif.py
@@ -2,11 +2,17 @@ import glob
 import pathlib
 from PIL import Image
 
-def make_gif(frame_folder):
+def make_gif(frame_folder : pathlib.Path):
     frames = [Image.open(image) for image in glob.glob(f"{frame_folder}/*.JPG")]
     frame_one = frames[0]
-    frame_one.save("my_awesome.gif", format="GIF", append_images=frames, save_all=True, duration=100, loop=0)
+
+    dest = frame_folder.joinpath("my_awesome.gif")
+
+    print(dest)
+
+    frame_one.save(dest, format="GIF", append_images=frames, save_all=True, duration=200, loop=0)
 
 if __name__ == "__main__":
     path = pathlib.Path(r'C:\Users\matze\Desktop\export\Kinder\gif')
+    
     make_gif(path)
